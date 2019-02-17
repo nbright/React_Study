38df72d20029ed93e8a0f231424ce028ed06cf5b
nbright <kms3700@gmail.com>
2019-02-17 23:45:47 +0900

    #최초 github test

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -10,14 +10,32 @@ const movieTitles = [
 ]
 
 const movieImages = [
-  "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg",
+
   "https://images-na.ssl-images-amazon.com/images/I/41MN0ANVJTL._SY445_.jpg",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXepaTQOs33GBlFS1Lu0Y6ifwsINileFw1TxhC62NId7VsRmxg",
   "https://images-na.ssl-images-amazon.com/images/I/81WjGytz7HL._SY445_.jpg"
 ]
 
+const movies = [
+  {
+    title: "Matrix",
+    poster:   "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg"
+  },
+  {
+    title: "Matrix",
+    poster:   "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg"
+  },
+  {
+    title: "Matrix",
+    poster:   "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg"
+  },{
+    title: "Matrix",
+    poster:   "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg"
+  },
+]
+
 class App extends Component {
-  // render() {
+   render() {
     return (
       <div className="App">
         <Movie title={movieTitles[0]} poster={movieImages[0]}/>
