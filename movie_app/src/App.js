be34f9018333684ddd57bb77c87f558ef898e489
nbright <kms3700@gmail.com>
2019-02-21 09:03:34 +0900

    Practicing this.setState()

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,30 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "Matrix",
-    poster:   "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg"
-  },
-  {
-    title: "Full Metal Jacket",
-    poster:   "https://images-na.ssl-images-amazon.com/images/I/41MN0ANVJTL._SY445_.jpg"
-  },
-  {
-    title: "Oldboy",
-    poster:   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXepaTQOs33GBlFS1Lu0Y6ifwsINileFw1TxhC62NId7VsRmxg"
-  },
-  {
-    title: "Star Wars",
-    poster:   "https://images-na.ssl-images-amazon.com/images/I/81WjGytz7HL._SY445_.jpg"
-  }
-]
+
 
 class App extends Component {
-   render() {
+
+  state = {
+    greeting: 'Hello!',
+    // :(콜론) 키: 값 쌍을 나타낸다. 아래는 movies 라는 키 값에 영화정보의 값 배열이 들어있네.
+    movies: [
+      {
+        title: "Matrix",
+        poster:   "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg"
+      },
+      {
+        title: "Full Metal Jacket",
+        poster:   "https://images-na.ssl-images-amazon.com/images/I/41MN0ANVJTL._SY445_.jpg"
+      },
+      {
+        title: "Oldboy",
+        poster:   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXepaTQOs33GBlFS1Lu0Y6ifwsINileFw1TxhC62NId7VsRmxg"
+      },
+      {
+        title: "Star Wars",
+        poster:   "https://images-na.ssl-images-amazon.com/images/I/81WjGytz7HL._SY445_.jpg"
+      }
+    ]
+  }
+
+  componentDidMount(){
+    setTimeout(() =>{
+      this.setState({
+        movies : [
+          ...this.state.movies,
+          {
+            title: "transporter",
+            poster: "https://images-na.ssl-images-amazon.com/images/I/51NnvDGf4hL._SX342_.jpg"
+          }
+        ]
+      }) 
+    },2000)
+  }
+
+  render() {
     return (
       <div className="App">
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>
