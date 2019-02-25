cbb507f417afd6b2340acea603254a8cfd28fbec
nbright <kms3700@gmail.com>
2019-02-25 09:04:25 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nbright/React_Study/tree/master/movie_app/precache-manifest.9731e04f39b4ad50a0fa7b5b9a07b05d.js"
+  "/movie_app/precache-manifest.533368e9d75a375c0e81b7ddca80857f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nbright/React_Study/tree/master/movie_app/index.html", {
+workbox.routing.registerNavigationRoute("/movie_app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
